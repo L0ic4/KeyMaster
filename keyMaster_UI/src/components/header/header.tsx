0e8a57d2ecf4d69a3e03a6eb0677876b4b0cdbee
NiceGuyMe <boy-unfind@proton.me>
2024-02-14 18:20:57 +0300

    update deps

--- keyMaster_UI/src/components/header/header.tsx
+++ keyMaster_UI/src/components/header/header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Header = () => {
@@ -6,7 +7,7 @@ const Header = () => {
       <nav className='border-gray-200 bg-white px-4 py-2.5 lg:px-6 dark:bg-gray-800'>
         <div className='mx-auto flex max-w-screen-xl flex-wrap items-center justify-between'>
           <a href='https://flowbite.com' className='flex items-center'>
-            <img
+            <Image
               src='https://flowbite.com/docs/images/logo.svg'
               className='mr-3 h-6 sm:h-9'
               alt='Flowbite Logo'
